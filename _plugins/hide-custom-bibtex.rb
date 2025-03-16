a514c18d217cd78da95f3fa89ca2bb8241023559
Julio Solis <52597364+juliosolisar@users.noreply.github.com>
2025-03-16 00:19:25 -0400

    Update hide-custom-bibtex.rb

--- _plugins/hide-custom-bibtex.rb
+++ _plugins/hide-custom-bibtex.rb
@@ -1,14 +1,19 @@
 module Jekyll
   module HideCustomBibtex
     def hideCustomBibtex(input)
-	    keywords = @context.registers[:site].config['filtered_bibtex_keywords']
+      # first remove any fields listed in _config.yml under 'filtered_bibtex_keywords'
+      keywords = @context.registers[:site].config['filtered_bibtex_keywords']
+      keywords.each do |keyword|
+        input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
+      end
 
-	    keywords.each do |keyword|
-		    input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
-	    end
+      # remove lines with the sortyear field
+      input = input.gsub(/^.*\bsortyear\b *= *\{.*$\n/, '')
 
       # Clean superscripts in author lists
-      input = input.gsub(/^.*\bauthor\b *= *\{.*$\n/) { |line| line.gsub(/[*†‡§¶‖&^]/, '') }
+      input = input.gsub(/^.*\bauthor\b *= *\{.*$\n/) do |line|
+        line.gsub(/[*†‡§¶‖&^]/, '')
+      end
 
       return input
     end
