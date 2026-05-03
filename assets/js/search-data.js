2f12df67f5ac6b49d6aff4b6afcffa28307047b0
juliosolisar <52597364+juliosolisar@users.noreply.github.com>
2026-05-03 21:26:11 +0000

    Deploying to gh-pages from @ juliosolisar/juliosolisar.github.io@6418b0eb616242cef916c7f5fdaea73d4973f2f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
+  },{id: "nav-bio",
+          title: "bio",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/bio/";
+          },
+        },{id: "nav-research",
           title: "research",
           description: "",
           section: "Navigation",
