0577df02be7cec1789401a635e33ebdbe915be73
juliosolisar <52597364+juliosolisar@users.noreply.github.com>
2026-05-03 21:36:01 +0000

    Deploying to gh-pages from @ juliosolisar/juliosolisar.github.io@8bb580be41fcb96182098ada20add29c45420461 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,6 +69,9 @@ ninja.data = [{
           section: "News",},{id: "news-i-attended-the-game-theory-political-economy-amp-amp-development-conference-hosted-at-université-mohammed-vi-polytechnique-um6p-in-morocco-check-the-program-here",
           title: 'I attended the Game Theory, Political Economy &amp;amp;amp; Development conference, hosted at Université...',
           description: "",
+          section: "News",},{id: "news-we-are-discussing-my-dissertation-project-at-the-boston-area-working-group-in-african-political-economy",
+          title: 'We are discussing my dissertation project at the Boston Area Working Group in...',
+          description: "",
           section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
